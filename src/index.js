d3036dac8ef4772512780cd9229add4808f3585d
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-15 12:43:33 -0500

    Use env for database host

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 app.use(express.json(), cors());
 
 const dbConfig = {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
